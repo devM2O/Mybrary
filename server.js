d376dad052970f3cef9e51324269a7c7cdca8385
Minn Maw Oo <minmawoo.ucsm@gmail.com>
2020-10-06 01:53:58 +0630

    initial authors routes

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express');
 const app = express()
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 //Mongoose
 const mongoose = require('mongoose');
@@ -21,6 +23,7 @@ mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true,useUnifiedTopo
 
 //Routes
 app.use('/', require('./routes/index'));
+app.use('/authors', require('./routes/authors'));
 
 
 const port = process.env.PORT || 3000;
